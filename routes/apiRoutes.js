e106e3bef819c098fa9ae653d519d9faa5f79503
ABojo <alex.bojorquez2598@gmail.com>
2021-06-10 00:46:55 -0400

    removed matches and stats routes + controllers as theyre now not needed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const statsRouter = require('./statsRoutes');
-const matchesRouter = require('./matchesRoutes');
 const platformRouter = require('./platformRoutes');
 
-router.use('/stats', statsRouter);
-router.use('/matches', matchesRouter);
 router.use('/platforms', platformRouter);
 
 module.exports = router;
