f4b7e8b2d327314bd35a7b74988b3c02613d89ba
ABojo <alex.bojorquez2598@gmail.com>
2021-06-09 22:40:01 -0400

    Built getMatches handler function

--- controllers/matchesController.js
+++ controllers/matchesController.js
@@ -1,1 +1,50 @@
-exports.getMatches = (req, res) => {};
+const catchAsync = require('../utils/catchAsync');
+
+exports.getMatches = catchAsync(async (req, res) => {
+  const { platform, username } = req.params;
+  const response = await req.api.getMatches(platform, username);
+
+  if (response.status === 'error')
+    throw new Error(
+      "Sorry, that profile either doesn't exist or their data is private!"
+    );
+
+  const matches = response.data.matches.map((match) => {
+    return {
+      matchDetails: {
+        startTime: match.utcStartSeconds,
+        endTime: match.utcEndSeconds,
+        mode: match.mode,
+        id: match.matchID,
+        duration: match.duration,
+        playerCount: match.playerCount,
+        teamCount: match.teamCount,
+      },
+      playerStats: {
+        kills: match.playerStats.kills,
+        deaths: match.playerStats.deaths,
+        kdRatio: match.playerStats.kdRatio,
+        damageDone: match.playerStats.damageDone,
+        damageTaken: match.playerStats.damageTaken,
+        headshots: match.playerStats.headshots,
+        assists: match.playerStats.assists,
+        scorePerMinute: match.playerStats.scorePerMinute,
+        totalXp: match.playerStats.totalXp,
+        distanceTraveled: match.playerStats.distanceTraveled,
+        percentTimeMoving: match.playerStats.percentTimeMoving,
+      },
+      playerInfo: {
+        username: match.player.username,
+        uno: match.player.uno,
+        clantag: match.player.clantag,
+      },
+    };
+  });
+
+  res.json({
+    status: 'success',
+    data: {
+      matches,
+    },
+  });
+});
--- routes/matchesRoutes.js
+++ routes/matchesRoutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const matchesController = require('../controllers/matchesController');
+const authController = require('../controllers/authController');
 
-router.get('/:platform/:username', matchesController.getMatches);
+router.get(
+  '/:platform/:username',
+  authController.isLoggedIn,
+  matchesController.getMatches
+);
 
 module.exports = router;
