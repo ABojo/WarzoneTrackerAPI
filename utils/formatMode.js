7789f9f083264c2de4b9f5064d6d74c66ed17bf4
ABojo <alex.bojorquez2598@gmail.com>
2021-06-13 12:53:06 -0400

    Added support for Rebirth in the format mode function

--- utils/formatMode.js
+++ utils/formatMode.js
@@ -1,5 +1,6 @@
 const formatMode = (mode) => {
   if (mode.includes('pln')) return 'Plunder';
+  if (mode.includes('rebirth')) return 'Rebirth';
 
   const str = mode.split('br_br')[1];
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
