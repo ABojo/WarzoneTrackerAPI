0341c6cae444390eeb327a3f9a37890689cdf73a
ABojo <alex.bojorquez2598@gmail.com>
2021-06-21 22:05:27 -0400

    Added an more user friendly error message if their request fails because the server hasnt yet authenticated with activisions

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,12 @@
 const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  const message = err.message.includes('.login()')
+    ? 'Sorry, this is a test app so our free server is waking up! Please resubmit the username to get the data.'
+    : err.message;
+
   res.json({
     status: 'error',
-    message: err.message,
+    message,
   });
 };
 
